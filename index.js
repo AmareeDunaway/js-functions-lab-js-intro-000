d038a6cd47a90bf7b128e3f00b321e7d9d746f56
amareedunaway <amaree.dunaway.2018@gmail.com>
2016-11-01 17:55:48 +0000

    I am ready to commit my files.

---
+++ index.js
@@ -0,0 +1,5 @@
+// write your code below!
+var happyHolidays = function () { return "Happy holidays!";}
+var happyHolidaysTo = function (name) { return "Happy holidays, " + name + "!";}
+var happyHolidayTo = function (holiday,name) { return "Happy Independence Day, " +  name  +  "!";}
+var holidayCountdown = function (holidays, days ) { return "It's " + days + " days until " + holidays + "!";}
